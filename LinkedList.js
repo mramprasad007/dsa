75c686bb54643e7385efd02355ef702b8bb09d24
Ram Prasad <mramprasad007@gmail.com>
2022-11-28 19:06:20 +0530

    Initial Commit

---
+++ LinkedList.js
@@ -0,0 +1,40 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(initialValue){
+        this.head = new Node(initialValue);
+        this.length = 1;
+        this.tail = this.head;
+    }
+    push(value){
+
+    }
+    pop(){
+        
+    }
+    set(index, value){
+
+    }
+    get(index){
+
+    }
+    unShift(value){
+
+    }
+    shift(){
+
+    }
+    insert(index, value){
+
+    }
+    reverse(){
+        
+    }
+}
+
+const linkedlist = new LinkedList(1);
